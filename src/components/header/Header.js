1c3264f942a30b877d21a29ef2d35a43d94cf81f
Ankit Raj <2019ugcs001@nitjsr.ac.in>
2020-10-07 17:38:22 +0530

    homeScreen

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.scss'
+function Header() {
+    return (
+    <div className='header'>
+        <h2>Dashboard/Home</h2>
+    </div>
+    )
+}
+
+export default Header
---
+++ src/screens/launchScreen/Launch.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Launch() {
+    return (
+        <div>
+            Launch
+        </div>
+    )
+}
+
+export default Launch
